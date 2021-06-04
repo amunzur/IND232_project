4cbd4e1aad936dba578780a744cae2e2113beb96
amunzur <admunzur.21@gmail.com>
2021-06-04 12:18:55 -0700

    updated such that the linewidth is considered while making the figures

--- scripts/utilities_make_oncoprint.py
+++ scripts/utilities_make_oncoprint.py
@@ -40,12 +40,16 @@ def plot_cn(samples, genes, df_cn, ax, offset, bar_height, bar_width):
             ax.bar(sample, bar_height, bottom = bottom, color = color, zorder = 10, width = bar_width * 1.2)
             bottom += 1
         
-def plot_muts(sample_pos, gene_pos, df_muts, ax):
+def plot_muts(sample_pos, gene_pos, df_muts, ax, offset_how):
     '''
     # Add mutations to the CN heatmap.
     '''
     
-    marker_size = 9
+    # map a dict
+    lw_dict = {"s": 0, "*": 1, "^": 1}
+    df_muts['lw_col'] = df_muts['shapes'].map(lw_dict)
+
+    marker_size = 30
     
     for i, row in df_muts.iterrows():
         sample = row['Sample']
@@ -57,23 +61,32 @@ def plot_muts(sample_pos, gene_pos, df_muts, ax):
             
         if df["Effect"].str.contains("Multiple somatic mutations").any():
             if len(df) == 1: 
-                ax.scatter(x = sample_pos[sample], y = gene_pos[gene], c = "black", s = marker_size + 3, marker = "^", zorder = 100, lw = 0)
+                ax.scatter(x = sample_pos[sample], y = gene_pos[gene] + 0.02, c = "black", s = marker_size + 3, marker = "^", zorder = 100, lw = 0)
         else:
             if len(df) == 1:
-                ax.scatter(x = sample_pos[sample], y = gene_pos[gene] - 0.06, c = color, s = marker_size, marker = str(df["shapes"][0]), zorder = 100, lw = 0)
+                if "Germline" in row["Effect"]:
+                    ax.scatter(x = sample_pos[sample], y = gene_pos[gene] + 0.02, c = color, s = marker_size, marker = str(df["shapes"][0]), zorder = 100, lw = 1)
+                else: 
+                    ax.scatter(x = sample_pos[sample], y = gene_pos[gene] + 0.02, c = color, s = marker_size, marker = str(df["shapes"][0]), zorder = 100, lw = 0)
 
             elif len(df) == 2: # two mutations
-                ax.scatter(x = sample_pos[sample] - 0.07, y = gene_pos[gene] - 0.08, c = df.loc[0, "Color"], s = marker_size, marker = df.loc[0, "shapes"], zorder = 100, lw = 0)
-                ax.scatter(x = sample_pos[sample] + 0.07, y = gene_pos[gene] + 0.02, c = df.loc[1, "Color"], s = marker_size, marker = df.loc[1, "shapes"], zorder = 100, lw = 0)
+                if offset_how == "vertical": 
+                    ax.scatter(x = sample_pos[sample], y = gene_pos[gene] - 0.17, c = df.loc[0, "Color"], s = marker_size, marker = df.loc[0, "shapes"], zorder = 100, lw = df.loc[0, "lw_col"])
+                    ax.scatter(x = sample_pos[sample], y = gene_pos[gene] + 0.17, c = df.loc[1, "Color"], s = marker_size, marker = df.loc[1, "shapes"], zorder = 100, lw = df.loc[1, "lw_col"])
+                else: 
+                    ax.scatter(x = sample_pos[sample] - 0.07, y = gene_pos[gene] - 0.03, c = df.loc[0, "Color"], s = marker_size, marker = df.loc[0, "shapes"], zorder = 100, lw = df.loc[0, "lw_col"])
+                    ax.scatter(x = sample_pos[sample] + 0.07, y = gene_pos[gene] + 0.05, c = df.loc[1, "Color"], s = marker_size, marker = df.loc[1, "shapes"], zorder = 100, lw = df.loc[1, "lw_col"])
                 
-            elif len(df) == 3: # three mutations3
-                ax.scatter(x = sample_pos[sample], y = gene_pos[gene], c = "black", s = marker_size + 3, marker = "^", zorder = 100, lw = 0)
+            elif len(df) >= 2: # three mutations3
+                ax.scatter(x = sample_pos[sample], y = gene_pos[gene] + 0.02, c = "black", s = marker_size + 3, marker = "^", zorder = 100, lw = 0)
             
 def plot_mut_and_cn_counts(df_cn, df_muts, drop): 
     '''
     Given a df_cn and df_muts data frames, plot the number of CN changes and mutation events in vertical bar graphs. 
     '''
-
+    # get a list of genes from df_cn 
+    genes = df_cn["Gene"].unique()
+    
     # from df cn, drop rows if CN is 0
     df_cn = df_cn.drop(df_cn[df_cn["Copy number"] == 0].index)
     df_cn = df_cn.groupby(['Gene']).size()
@@ -90,7 +103,16 @@ def plot_mut_and_cn_counts(df_cn, df_muts, drop):
     combined.columns = ["CN_changes", "Mutation_events"]
     
     combined = combined.fillna(0)
-
+    
+    #add the genes we lost above 
+    for gene in genes: 
+        if gene not in combined.index:      
+            row = pd.DataFrame({'Gene': [gene],
+                   'CN_changes': [0],
+                   'Mutation_events': [0]}).set_index("Gene")
+            
+            combined = pd.concat([combined, row])
+        
     return combined
 
 def filter_df_by_col(df, colname):
@@ -121,17 +143,32 @@ def convert_counts_to_percentage(CN_filter, mut_filter, AR_filter, df_cn, df_cou
     df_counts["CN_changes_perc"] = round(df_counts.CN_changes/CN_filt * 100)
     df_counts["Mutation_events_perc"] = round(df_counts.CN_changes/mut_filt * 100)
         
-    # deal with AR
-    AR_filt = len(dups.loc[dups["ctDNA fraction"] > AR_filter])
-    df_counts.loc["AR", "CN_changes_perc"] = round((df_counts.loc["AR", "CN_changes"] / AR_filt) * 100)
-    df_counts.loc["AR", "Mutation_events_perc"] = round((df_counts.loc["AR", "Mutation_events"] / AR_filt) * 100)
+    # deal with AR, if it's in the df indices
+    if df_counts.index.str.contains("AR").any(): 
+        AR_filt = len(dups.loc[dups["ctDNA fraction"] > AR_filter])
+        df_counts.loc["AR", "CN_changes_perc"] = round((df_counts.loc["AR", "CN_changes"] / AR_filt) * 100)
+        df_counts.loc["AR", "Mutation_events_perc"] = round((df_counts.loc["AR", "Mutation_events"] / AR_filt) * 100)
     
     return df_counts
     
     
+def filter_by_genes(df, repair, other): 
+    '''
+    Given a df_cn and muts data frames, separate them based on genes. One df will be DNA repair genes, other will be the rest. 
+    Two data frames are returned. 
+    '''
+    
+    idx_repair = df["Gene"].isin(repair)
+    idx_other = df["Gene"].isin(other)
+    
+    df_repair = df[idx_repair]
+    df_other = df[idx_other]
+        
+    return[df_repair, df_other]
+
     
     
-    
+
     
     
     
